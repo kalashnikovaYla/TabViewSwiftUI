1186781a1566a3b39d99c2505ba296a75d9545b9
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 14:09:03 +0500

    Initial Commit

---
+++ TabViewSwiftUI/TabViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewSwiftUIApp.swift
+//  TabViewSwiftUI
+//
+//  Created by sss on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TabViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
